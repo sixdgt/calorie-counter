a4cd97ff980db9df7519706f7e55d7d74fd48650
sixdgt <c4crypt@gmail.com>
2022-06-14 14:25:27 +0545

    template layouts, login-registration

---
+++ caloriecounter/ccapp/forms.py
@@ -0,0 +1,22 @@
+from django import forms
+
+
+from ccapp.models import AppUser
+
+class LoginForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput)
+    class Meta:
+        # if we need all fields of model
+        # fields = "__all__"
+
+        # for login we only require email and password
+        fields = ('email', 'password')
+        model = AppUser
+
+class RegistrationForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput)
+    class Meta:
+        fields = ('first_name', 'middle_name', 'last_name', 'contact', 'email',\
+            'gender', 'dob', 'blood_group', 'password')
+
+        model = AppUser
--- caloriecounter/ccapp/views.py
+++ caloriecounter/ccapp/views.py
@@ -1,5 +1,54 @@
 from django.shortcuts import render
+from ccapp.forms import RegistrationForm
+
+from ccapp.forms import LoginForm
 
 # Create your views here.
+def landing(request):
+    template = 'index.html'
+    context = {
+        'page_content_title': 'Home Page',
+        'msg_welcome': 'Welcome to calorie counter.'
+    }
+    return render(request, template, context)
+
+def user_login(request):        
+    login_form = LoginForm()
+    template = 'users/login.html'
+    if request.method == "POST":
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+
+        context = {
+            'form': login_form,
+            'data': {
+                    'email': email,
+                    'password': password
+                }
+            }
+        return render(request, template, context)
+    else:
+        context = {'form': login_form}
+        return render(request, template, context)
+        
+
+def user_register(request):
+    register_form = RegistrationForm()
+    template = 'users/create.html'
+    context = {'form': register_form}
+    return render(request, template, context)
+
+
 def user_index(request):
-    return render(request, 'users/index.html')
+    # render() - use to render the templates/pages
+    # this function takes three parameter
+    # 1. request - request from the client
+    # 2. template - html page direction
+    # 3. context - data that are passed to templates ( must be of type dict ) - is optional
+    template = 'users/index.html'
+    context = {
+        'page_content_title': 'User Dashboard',
+        'page_content_body': 'Welcome to calorie counter.'
+    }
+
+    return render(request, template, context)
